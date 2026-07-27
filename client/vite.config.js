72b8bda8a5cd99b16b125fc5663883221a92bdde
web-coder <yasher18@bk.ru>
2026-07-27 16:55:21 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import dns from 'dns'
-
-
-dns.setDefaultResultOrder('verbatim');
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,7 +17,6 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 4000,
-    // allowedHosts: ['leadorub.hbnetwork.ru'],
+    port: 4000
   }
 })
