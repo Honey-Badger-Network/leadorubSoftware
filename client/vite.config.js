93623c8e49e9162d0b1fd006e7fcb5b579af8a8c
web-coder <yasher18@bk.ru>
2026-07-27 15:39:54 +0300

    ss

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dns from 'dns'
 
+
+dns.setDefaultResultOrder('verbatim');
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,6 +21,7 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 4000
+    port: 4000,
+    // allowedHosts: ['leadorub.hbnetwork.ru'],
   }
 })
